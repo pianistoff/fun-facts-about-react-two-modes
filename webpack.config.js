974b0caadd53cc7ff5ad3b9674c06e16da3e7572
pianistoff <severyn.h@gmail.com>
2022-07-05 22:59:31 +0300

    Add functionality to the dark mode toggle

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./index.js",
+    output: {
+        path: path.resolve("public"),
+        filename: "main.js",
+    },
+
+    target: "web",
+    devServer: {
+        port: "3000",
+
+        static: ["./public"],
+
+        open: true,
+
+        hot: true,
+
+        liveReload: true,
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json"],   
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: "babel-loader",
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                include: [
+                    path.resolve("src/img/html")
+                ],
+                loader: 'file-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+              }
+        ]
+    }
+}
